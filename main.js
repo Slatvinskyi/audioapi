1bbf3154ffee2036254e572e803f80f64f338fba
Slatvinskyi <andrii.slatvinskyi@gmail.com>
2021-10-06 13:25:10 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const ctx = new AudioContext();
+let audio;
+
+fetch("./audio/ukulele.mp3")
+.then(data => data.arrayBuffer())
+.then(arrayBuffer => ctx.decodeAudioData(arrayBuffer)) 
+.then(decodedAudio => {
+    audio = decodedAudio;
+});
+
+function playback() {
+    const playSound = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
+
+}
+
+window.addEventListener("mousedown", playback);
